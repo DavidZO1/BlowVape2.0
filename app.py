8b571a9c38765eddb701fad069da10efac5db041
DavidZO1 <david.zapataob@upb.edu.co>
2023-11-02 14:54:49 -0500

    intentando hacer cosas

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS  # Import the CORS extension
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blowvape.db'  # Use SQLite for simplicity
+db = SQLAlchemy(app)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {'message': 'Hola desde Flask!'}
-    return jsonify(data)
+# Define the Product model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    price = db.Column(db.Float)
+    image_url = db.Column(db.String(200))
+
+# Define API endpoints for product data
+@app.route('/app.py', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    product_list = [{'id': product.id, 'name': product.name, 'description': product.description, 'price': product.price, 'image_url': product.image_url} for product in products]
+    return jsonify(product_list)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
--- my-app/src/pages/home/PopularProducts.tsx
+++ my-app/src/pages/home/PopularProducts.tsx
@@ -1,25 +1,32 @@
-import { popularProducts} from "../../assets/data/popular";
-import {Product} from "../../assets/data/popular";
+import React, { useState, useEffect } from 'react';
 
-const PopularProducts = ():JSX.Element =>{
-    return(
-        <div className='popular-products'>
-            <h1>Popular Products</h1>
-            <div className="product-items">
-                {
-                    popularProducts.map((item:Product) =>{
-                        return(
-                            <div className='product-item' key={item.id}>
-                                <img src={item.img} alt={item.description}/>
-                                <p>{item.description}</p>
-                                <h6><strong>{item.price}</strong></h6>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+const PopularProducts = () => {
+  const [popularProducts, setPopularProducts] = useState([]);
 
-export default  PopularProducts;
+  useEffect(() => {
+    // Fetch popular products from your Flask backend
+    fetch('/app.py') // You may need to adjust the URL depending on your setup
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data))
+      .catch((error) => console.error('Error fetching popular products:', error));
+  }, []);
+
+  return (
+    <div className='popular-products'>
+      <h1>Popular Products</h1>
+      <div className='product-items'>
+        {popularProducts.map((item) => (
+          <div className='product-item' key={item.id}>
+            <img src={item.image_url} alt={item.description} />
+            <p>{item.description}</p>
+            <h6>
+              <strong>{item.price}</strong>
+            </h6>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularProducts;
