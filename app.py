65419dde51709d7874d94d1589067004975b1560
SantiagoHB <santiago.hernandezl@outlook.com>
2023-11-02 21:50:23 -0500

    Arreglo PopularProducts y Appy.py con MYSQL

--- app.py
+++ app.py
@@ -1,31 +1,62 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+#from flask import Flask, request, jsonify
+#from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS  # Import the CORS extension
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
+
+#app = Flask(__name__)
+#CORS(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blowvape.db'  # Use SQLite for simplicity
+#db = SQLAlchemy(app)
+
+# Define the Product model
+#class Product(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(100))
+#    description = db.Column(db.Text)
+#    price = db.Column(db.Float)
+#    image_url = db.Column(db.String(200))
+
+# Define API endpoints for product data
+#@app.route('/app.py', methods=['GET'])
+#def get_products():
+#    products = Product.query.all()
+#    product_list = [{'id': product.id, 'name': product.name, 'description': product.description, 'price': product.price, 'image_url': product.image_url} for product in products]
+#    return jsonify(product_list)
+
+#if __name__ == '__main__':
+#    with app.app_context():
+#        db.create_all()
+#    app.run(debug=True)
+
+#from flask import Flask, request, jsonify
+#from flask_cors import CORS
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blowvape.db'  # Use SQLite for simplicity
-db = SQLAlchemy(app)
 
-# Define the Product model
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    description = db.Column(db.Text)
-    price = db.Column(db.Float)
-    image_url = db.Column(db.String(200))
+# Configura la conexión a la base de datos MySQL
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root@localhos'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'vapes'
+
+# Inicializa la extensión MySQL
+mysql = MySQL(app)
 
 # Define API endpoints for product data
-@app.route('/app.py', methods=['GET'])
+@app.route('/products', methods=['GET'])
 def get_products():
-    products = Product.query.all()
-    product_list = [{'id': product.id, 'name': product.name, 'description': product.description, 'price': product.price, 'image_url': product.image_url} for product in products]
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM products')
+    products = cursor.fetchall()
+    cursor.close()
+
+    product_list = [{'id': product['id'], 'name': product['name'], 'description': product['description'], 'price': product['price'], 'image_url': product['image_url']} for product in products]
+
     return jsonify(product_list)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
 
--- my-app/src/pages/home/PopularProducts.tsx
+++ my-app/src/pages/home/PopularProducts.tsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
+// Define una interfaz para el tipo de datos esperados en popularProducts
+interface Product {
+  id: number;
+  image_url: string;
+  description: string;
+  price: number;
+}
+
 const PopularProducts = () => {
-  const [popularProducts, setPopularProducts] = useState([]);
+  const [popularProducts, setPopularProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     // Fetch popular products from your Flask backend
-    fetch('/app.py') // You may need to adjust the URL depending on your setup
+    fetch('/products') // Puedes ajustar la URL según tu configuración
       .then((response) => response.json())
       .then((data) => setPopularProducts(data))
       .catch((error) => console.error('Error fetching popular products:', error));
